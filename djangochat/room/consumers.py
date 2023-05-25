26a812426bd46591a9247c5aeb413195bc586a7f
Debian <debian@debian10.linuxvmimages.local>
2023-05-25 08:54:25 -0500

    create: feature send msg

--- djangochat/room/consumers.py
+++ djangochat/room/consumers.py
@@ -15,8 +15,35 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
         await self.accept()
         
-    async def disconnect(self):
+    async def disconnect(self, test_close):
         await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
         )
+
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+        message = data['message']
+        username = data['username']
+        room = data['room']
+
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': message,
+                'username': username,
+                'room': room,
+            }
+        )
+    
+    async def chat_message(self, event):
+        message = event['message']
+        username = event['username']
+        room = event['room']
+
+        await self.send(text_data=json.dumps({
+                'message': message,
+                'username': username,
+                'room': room,
+        }))
