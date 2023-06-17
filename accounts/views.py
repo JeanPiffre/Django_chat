3492ee42e94529055d906065d705706384731690
JeanPiffre <jb21.fave@gmail.com>
2023-06-17 23:02:06 +0200

    add: arborescence key crypto

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,8 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login
 from accounts.forms import UserRegisterForm
 
+from crypto import crypto_arbo
+from crypto import crypto_rsa
 
 def register(request):
     if request.method == "POST":
@@ -11,6 +13,9 @@ def register(request):
             form.save()
             username = form.cleaned_data.get("username")
             raw_password = form.cleaned_data.get("password1")
+            crypto_arbo.create_folder()
+            crypto_arbo.create_files(username)
+            crypto_rsa.generate_keys(username)
             user = authenticate(username=username, password=raw_password)
             login(request, user)
             messages.success(
